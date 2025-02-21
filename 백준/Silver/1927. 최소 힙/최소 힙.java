4733cd4772fbde49754f53cfdc546327cb93877f
solar <64721658+jung980514@users.noreply.github.com>
2025-02-21 14:18:21 +0900

    [Silver II] Title: 최소 힙, Time: 1008 ms, Memory: 33952 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException{
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		int n = Integer.parseInt(br.readLine());
+		for(int i=0;i<n;i++) {
+			int x = Integer.parseInt(br.readLine());
+			if(x==0) {
+				if(pq.isEmpty()) {
+					System.out.println(0);
+				}
+				else {
+					System.out.println(pq.poll());
+				}
+			}
+			else {
+				pq.add(x);
+			}
+		}
+	}
+}
