f924691a2b90114d6e526d0ba6132d2d35783a18
solar <64721658+jung980514@users.noreply.github.com>
2025-02-27 00:23:19 +0900

    [Gold V] Title: 캠프 준비, Time: 68 ms, Memory: 11704 KB -BaekjoonHub

--- 백준/Gold/16938. 캠프 준비/캠프 준비.java
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.java
@@ -24,17 +24,20 @@ public class Main {
 		System.out.println(result);
 	}
 	public static void SelectProblem(int cnt,int sum,int count,int min,int max) {
+		//합이 sum보다 크면 그다음 가지 안봐도 됨
 		if(sum>r) {
 			return;
 		}
+		//다 선택하면 주어진 조건에 맞을시 result++
 		if(cnt==n) {
 			if(count>=2 && sum>=l && max-min>=x) {
 				result++;
 			}
 			return;
 		}
+		//선택했을때
 		SelectProblem(cnt+1,sum+problem[cnt],count+1,Math.min(min,problem[cnt]),Math.max(max,problem[cnt]));
-		
+		//선택하지 않았을때 
 		SelectProblem(cnt+1,sum,count,min,max);
 		
 	}
