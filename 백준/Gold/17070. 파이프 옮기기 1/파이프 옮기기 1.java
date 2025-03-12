26adc91d2e049378b74ad63adeb5bfb04c84ae8d
solar <64721658+jung980514@users.noreply.github.com>
2025-03-12 10:51:42 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 312 ms, Memory: 16516 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -19,24 +19,24 @@ public class Main {
 			}
 		}
 		int flag = 0; // 0:가로 1:대각선  2:세로
+		//시작 파이프 위치 방문처리
 		visited[0][0]=true;
 		visited[0][1]=true;
+		
 		dfs(0,1,flag,n);
 		System.out.println(result);
 	}
 	public static void dfs(int x,int y,int flag,int n) {
-		if(x==n-1 && y==n-1) {
+		if(x==n-1 && y==n-1) { // (n,n)값이면 방법 수 +1
 			result++;
 			return;
 		}
-		if(flag==0) {
+		if(flag==0) { //가로일때
 			for(int i=0;i<2;i++) {
 				int nx = x+dx[i];
 				int ny = y+dy[i];
-				if(i==1) {
-					if(isRange(nx,ny,n) && !visited[nx][y] && !visited[x][ny] 
-							&& !visited[nx][ny] && map[nx][ny]==0 && map[nx][y]==0
-									&& map[x][ny]==0) {
+				if(i==1) { //대각선일때는 (nx,y),(x,ny),(nx,ny) 3개를 다 비교해야한다
+					if(isDiag(x,y,nx,ny,n)) {
 						visited[nx][ny] = true;
 						visited[nx][y] = true;
 						visited[x][ny] = true;
@@ -55,14 +55,12 @@ public class Main {
 				}
 			}
 		}
-		else if(flag==1) {
+		else if(flag==1) { //대각선일때
 			for(int i=0;i<3;i++) {
 				int nx = x+dx[i];
 				int ny = y+dy[i];
-				if(i==1) {
-					if(isRange(nx,ny,n) && !visited[nx][y] && !visited[x][ny] 
-							&& !visited[nx][ny] && map[nx][ny]==0 && map[nx][y]==0
-									&& map[x][ny]==0) {
+				if(i==1) { //대각선일때는 (nx,y),(x,ny),(nx,ny) 3개를 다 비교해야한다
+					if(isDiag(x,y,nx,ny,n)) {
 						visited[nx][ny] = true;
 						visited[nx][y] = true;
 						visited[x][ny] = true;
@@ -81,14 +79,12 @@ public class Main {
 				}
 			}
 		}
-		else {
+		else { // 세로일때
 			for(int i=1;i<3;i++) {
 				int nx = x+dx[i];
 				int ny = y+dy[i];
-				if(i==1) {
-					if(isRange(nx,ny,n) && !visited[nx][y] && !visited[x][ny] 
-							&& !visited[nx][ny] && map[nx][ny]==0 && map[nx][y]==0
-									&& map[x][ny]==0) {
+				if(i==1) { //대각선일때는 (nx,y),(x,ny),(nx,ny) 3개를 다 비교해야한다
+					if(isDiag(x,y,nx,ny,n)) {
 						visited[nx][ny] = true;
 						visited[nx][y] = true;
 						visited[x][ny] = true;
@@ -108,8 +104,14 @@ public class Main {
 			}
 		}
 	}
-	public static boolean isRange(int x,int y,int n) {
+	public static boolean isRange(int x,int y,int n) { //nx와 ny값이 범위안인지 판별
 		if(x>=0 && y>=0 && x<n && y<n) return true;
+		else return false;
+	}
+	public static boolean isDiag(int x,int y,int nx,int ny,int n) { // 대각선이 가능한지 판별
+		if(isRange(nx,ny,n) && !visited[nx][y] && !visited[x][ny] 
+				&& !visited[nx][ny] && map[nx][ny]==0 && map[nx][y]==0
+						&& map[x][ny]==0) return true;
 		else return false;
 	}
 }
