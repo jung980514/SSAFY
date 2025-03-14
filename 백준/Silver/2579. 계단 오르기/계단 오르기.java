23276ae75fa7aaa066de543bf8a0e562134d6ce4
solar <64721658+jung980514@users.noreply.github.com>
2025-03-14 17:48:50 +0900

    [Silver III] Title: 계단 오르기, Time: 68 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -9,17 +9,20 @@ public class Main {
 		for(int i=1;i<=n;i++) {
 			stairs[i] = Integer.parseInt(br.readLine());
 		}
-		
+		//dp[0]은 1번뛸때 계산 dp[1]은 2번뛸때 계산
 		int[][] dp = new int[2][n+1];
 		dp[0][0] = 0;
 		dp[1][0] = 0;
 		dp[0][1] = stairs[1];
 		dp[1][1] = 0;
 		if(n>=2) {
+            //시작점 제외하고 한번뛰기를 연속으로 하면 안되기 때문에 따로 처리
 			dp[0][2] = dp[0][1]+stairs[2];
 			dp[1][2] = dp[1][0]+stairs[2];
 			for(int i=3;i<=n;i++) {
+                //1번뛰기를 연속으로 할 수 없으므로 이전에 2번뛰었을때 다음 한번뛰기
 				dp[0][i] = dp[1][i-1] + stairs[i];
+                //2번뛸때는 그 전값이 1번뛰든 2번뛰든 상관없으므로 둘중 최대값 선택 
 				dp[1][i] = Math.max(dp[0][i-2], dp[1][i-2]) + stairs[i];
 			}			
 		}
