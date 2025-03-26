7d9608960885abbb0240c20a57a639d5f6bfc740
solar <64721658+jung980514@users.noreply.github.com>
2025-03-26 12:21:38 +0900

    [Gold V] Title: 집합의 표현, Time: 1468 ms, Memory: 71008 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int[] parent;
+	static void union(int x, int y) {
+		int a = find(x);
+		int b = find(y);
+		
+		if( a > b ) {
+			parent[a] = b;
+		}
+		else {
+			parent[b] = a;
+		}
+	}
+	
+	static int find(int n) {
+		if(parent[n] != n)
+			parent[n] = find(parent[n]);
+		return parent[n];
+	}
+	public static void main(String[] args) throws IOException{
+		String[] s = br.readLine().split(" ");
+		int n = Integer.parseInt(s[0]);
+		int m = Integer.parseInt(s[1]);
+		parent = new int[n+1];
+		for(int j=1;j<=n;j++) {
+			parent[j] = j;
+		}
+		for(int j=0;j<m;j++) {
+			String[] s2 = br.readLine().split(" ");
+			if(s2[0].equals("0")) {
+				union(Integer.parseInt(s2[1]), Integer.parseInt(s2[2]));
+			}
+			else {
+				if(find(Integer.parseInt(s2[1])) == find(Integer.parseInt(s2[2])))
+					System.out.println("YES");
+				else
+					System.out.println("NO");
+			}
+		}
+	}
+	
+}
