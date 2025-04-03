5628d8cbf04be08dfefb9420f69099fe476b8ad9
solar <64721658+jung980514@users.noreply.github.com>
2025-04-03 15:31:00 +0900

    [Silver III] Title: 1로 만들기, Time: 100 ms, Memory: 15732 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,37 +1,26 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        // 입력 받기
-        int N = sc.nextInt();
-        
-        // dp 배열 초기화
-        int[] dp = new int[N + 1];
-        
-        // dp[1]은 0, 1에서 1로 가는 데 필요한 연산은 0번
-        dp[1] = 0;
-        
-        // 2부터 N까지 최소 연산 횟수 계산
-        for (int i = 2; i <= N; i++) {
-            // 1을 빼는 경우
-            dp[i] = dp[i - 1] + 1;
-            
-            // 2로 나누어 떨어지면
-            if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-            }
-            
-            // 3으로 나누어 떨어지면
-            if (i % 3 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-            }
-        }
-        
-        // 결과 출력
-        System.out.println(dp[N]);
-        
-        sc.close();
-    }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException{
+		int n = Integer.parseInt(br.readLine());
+		int dp[] = new int[1000001];
+		dp[1] = 0;
+		dp[2] = 1;
+		dp[3] = 1;
+		for(int i=4;i<=n;i++) {
+			if(i%2==0 && i%3==0)
+				dp[i] = Math.min(Math.min(dp[i-1]+1,dp[i/2]+1),dp[i/3]+1);
+			else if(i%2==0){
+				dp[i] = Math.min(dp[i-1]+1,dp[i/2]+1);
+			}
+			else if(i%3==0) {
+				dp[i] = Math.min(dp[i-1]+1,dp[i/3]+1);
+			}
+			else {
+				dp[i] = dp[i-1] +1;
+			}
+		}
+		System.out.println(dp[n]);
+	}
 }
