e9ad91ea840846183199b988b235e2fc87a5bace
solar <64721658+jung980514@users.noreply.github.com>
2025-04-08 15:50:42 +0900

    [Unrated] Title: [모의 SW 역량테스트] 핀볼 게임, Time: 2,748 ms, Memory: 120,704 KB -BaekjoonHub

--- SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.java
+++ SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.*;
 /**
  * 문제 분석
@@ -16,112 +17,159 @@ import java.util.*;
  *
  */
 public class Solution {
-    static int[] dx = {-1, 1, 0, 0}; // 상, 하, 좌, 우
-    static int[] dy = {0, 0, -1, 1};
-    static int[][] map;
-    static int[][] wormhole;
-    static int n, score, result;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+					 //상,하,좌,우
+	static int[] dx = {-1,1,0,0};
+	static int[] dy = {0,0,-1,1};
+	static int[][] map;
+	static int[][] wormhole;
+	static int n,score,result;
+	public static void main(String[] args) throws IOException{
+		int T = Integer.parseInt(br.readLine().trim());
+		for(int t=1;t<=T;t++) {
+			n = Integer.parseInt(br.readLine().trim());
+			map = new int[n+2][n+2];
+			wormhole = new int[5][4];
+			result=0;
+			for(int i=0;i<n+2;i++) {
+				Arrays.fill(map[i], 5);
+			}
+			for(int i=1;i<=n;i++) {
+				String[] s = br.readLine().trim().split(" ");
+				for(int j=1;j<=n;j++) {
+					map[i][j] = Integer.parseInt(s[j-1]);
+					if(map[i][j]>=6) {
+						if(wormhole[map[i][j]-6][0]==0) {
+							wormhole[map[i][j]-6][0] = i;
+							wormhole[map[i][j]-6][1] = j;							
+						}
+						else {
+							wormhole[map[i][j]-6][2] = i;
+							wormhole[map[i][j]-6][3] = j;
+						}
+					}
+				}
+			}
+			for(int i=1;i<=n;i++) {
+				for(int j=1;j<=n;j++) {
+					if(map[i][j]==0) {
+						for(int dir=0;dir<4;dir++) {
+							score = 0;
+							GameStart(i,j,dir);
+							result = Math.max(result, score);
+						}
+					}
+				}
+			}
+			System.out.println("#"+t+" "+result);
+		}
+	}
+	public static void GameStart(int i,int j,int dir) {
+		Queue<int[]> q = new ArrayDeque<int[]>();
+		q.offer(new int[] {i,j,dir});
+		
+		while(!q.isEmpty()) {
+			int[] tmp = q.poll();
+			int nx = tmp[0]+dx[tmp[2]];
+			int ny = tmp[1]+dy[tmp[2]];
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for (int t = 1; t <= T; t++) {
-            n = sc.nextInt();
-            map = new int[n + 2][n + 2];
-            wormhole = new int[5][4];
-            result = 0;
+			if(nx==i && ny==j)//초기위치
+				break;
+			
+			if(map[nx][ny]==-1)//블랙홀
+				break;
+			
+			if(map[nx][ny]==0) { //빈공간
+				q.offer(new int[] {nx,ny,tmp[2]});
+			}
+			else if(map[nx][ny]==1) {//1번 블록
+				int ndir = block(1,tmp[2]);
+				score++;
+				q.offer(new int[] {nx,ny,ndir});
+			}
+			else if(map[nx][ny]==2) {//2번 블록
+				int ndir = block(2,tmp[2]);
+				score++;
+				q.offer(new int[] {nx,ny,ndir});
+			}
+			else if(map[nx][ny]==3) {//3번 블록
+				int ndir = block(3,tmp[2]);
+				score++;
+				q.offer(new int[] {nx,ny,ndir});
+			}
+			else if(map[nx][ny]==4) {//4번 블록
+				int ndir = block(4,tmp[2]);
+				score++;
+				q.offer(new int[] {nx,ny,ndir});
+			}
+			else if(map[nx][ny]==5) {//5번 블록
+				int ndir = block(5,tmp[2]);
+				score++;
+				q.offer(new int[] {nx,ny,ndir});
+			}
+			else {//웜홀
+				if(wormhole[map[nx][ny]-6][0]==nx && wormhole[map[nx][ny]-6][1]==ny) {
+					q.offer(new int[] {wormhole[map[nx][ny]-6][2],wormhole[map[nx][ny]-6][3],tmp[2]});
+				}
+				else {
+					q.offer(new int[] {wormhole[map[nx][ny]-6][0],wormhole[map[nx][ny]-6][1],tmp[2]});
+				}
+			}
+		}
+	}
+	public static int block(int n, int dir) {
+		//0:상 1:하 2:좌 3:우
+		if(n==1) {
+			if(dir==0) {
+				return 1;
+			}
+			else if(dir==1)
+				return 3;
+			else if(dir==2)
+				return 0;
+			else
+				return 2;
+		}
+		else if(n==2) {
+			if(dir==0)
+				return 3;
+			else if(dir==1)
+				return 0;
+			else if(dir==2)
+				return 1;
+			else
+				return 2;
+		}
+		else if(n==3) {
+			if(dir==0)
+				return 2;
+			else if(dir==1)
+				return 0;
+			else if(dir==2)
+				return 3;
+			else
+				return 1;
+		}
+		else if(n==4){
+			if(dir==0)
+				return 1;
+			else if(dir==1)
+				return 2;
+			else if(dir==2)
+				return 3;
+			else
+				return 0;
+		}
+		else {
+			if(dir==0)
+				return 1;
+			else if(dir==1)
+				return 0;
+			else if(dir==2)
+				return 3;
+			else
+				return 2;
+		}
+	}
 
-            for (int i = 0; i < n + 2; i++) {
-                Arrays.fill(map[i], 5); // 벽으로 초기화
-            }
-
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    map[i][j] = sc.nextInt();
-                    if (map[i][j] >= 6) {
-                        if (wormhole[map[i][j] - 6][0] == 0) {
-                            wormhole[map[i][j] - 6][0] = i;
-                            wormhole[map[i][j] - 6][1] = j;
-                        } else {
-                            wormhole[map[i][j] - 6][2] = i;
-                            wormhole[map[i][j] - 6][3] = j;
-                        }
-                    }
-                }
-            }
-
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    if (map[i][j] == 0) {
-                        for (int dir = 0; dir < 4; dir++) {
-                            score = 0;
-                            GameStart(i, j, dir);
-                            result = Math.max(result, score);
-                        }
-                    }
-                }
-            }
-
-            System.out.println("#" + t + " " + result);
-        }
-        sc.close();
-    }
-
-    public static void GameStart(int i, int j, int dir) {
-        Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{i, j, dir});
-
-        while (!q.isEmpty()) {
-            int[] tmp = q.poll();
-            int nx = tmp[0] + dx[tmp[2]];
-            int ny = tmp[1] + dy[tmp[2]];
-
-            if (nx == i && ny == j) break; // 출발 위치로 돌아옴
-            if (map[nx][ny] == -1) break; // 블랙홀 도달
-
-            if (map[nx][ny] == 0) {
-                q.offer(new int[]{nx, ny, tmp[2]});
-            } else if (map[nx][ny] >= 1 && map[nx][ny] <= 5) {
-                int ndir = block(map[nx][ny], tmp[2]);
-                score++;
-                q.offer(new int[]{nx, ny, ndir});
-            } else { // 웜홀
-                int wormNum = map[nx][ny] - 6;
-                if (wormhole[wormNum][0] == nx && wormhole[wormNum][1] == ny) {
-                    q.offer(new int[]{wormhole[wormNum][2], wormhole[wormNum][3], tmp[2]});
-                } else {
-                    q.offer(new int[]{wormhole[wormNum][0], wormhole[wormNum][1], tmp[2]});
-                }
-            }
-        }
-    }
-
-    public static int block(int n, int dir) {
-        if (n == 1) {
-            if (dir == 0) return 1;
-            else if (dir == 1) return 3;
-            else if (dir == 2) return 0;
-            else return 2;
-        } else if (n == 2) {
-            if (dir == 0) return 3;
-            else if (dir == 1) return 0;
-            else if (dir == 2) return 1;
-            else return 2;
-        } else if (n == 3) {
-            if (dir == 0) return 2;
-            else if (dir == 1) return 0;
-            else if (dir == 2) return 3;
-            else return 1;
-        } else if (n == 4) {
-            if (dir == 0) return 1;
-            else if (dir == 1) return 2;
-            else if (dir == 2) return 3;
-            else return 0;
-        } else {
-            if (dir == 0) return 1;
-            else if (dir == 1) return 0;
-            else if (dir == 2) return 3;
-            else return 2;
-        }
-    }
 }
