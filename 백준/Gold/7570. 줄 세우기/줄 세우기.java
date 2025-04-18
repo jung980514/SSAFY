18e365b7fb34832231984958ec0bf6e482377c8c
solar <64721658+jung980514@users.noreply.github.com>
2025-04-18 10:21:23 +0900

    [Gold II] Title: 줄 세우기, Time: 456 ms, Memory: 141380 KB -BaekjoonHub

---
+++ 백준/Gold/7570. 줄 세우기/줄 세우기.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException{
+		int n = Integer.parseInt(br.readLine());
+		String[] s = br.readLine().split(" ");
+		int[] child = new int[n+1];
+		for(int i=1;i<=n;i++) {
+			child[i] = Integer.parseInt(s[i-1]);
+		}
+		int[] dp = new int[n+1];
+		int max = 0;
+		for(int i=1;i<=n;i++) {
+			dp[child[i]] = dp[child[i]-1]+1;
+			max = Math.max(max, dp[child[i]]);
+		}
+		System.out.println(n-max);
+	}
+}
