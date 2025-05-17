140e18ec519cd8c904c89d1547ac37d44a01e535
solar <64721658+jung980514@users.noreply.github.com>
2025-05-17 12:20:28 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 64 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        String id = br.readLine();
+
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + id + "::fan:");
+        System.out.println(":fan::fan::fan:");
+    }
+}
